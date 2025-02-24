28e5b33d95b846766ffc6f8ce5240bb7b42f7ab8
Cyril <cyril@gmail.com>
2025-02-24 15:17:29 +0800

    first commit

---
+++ app/Http/Controllers/StudentsController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Students;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class StudentsController extends Controller
+{
+    public function index()
+    {
+        $students = Students::all();
+        return view('studentList', compact('students'));
+    }
+
+    public function newStudent(Request $request)
+    {
+        $request->validate([
+            'stdName' => 'required|max:255',
+            'stdAge' => 'required|numeric',
+        ]);
+
+        $input['name'] = $request->stdName;
+        $input['age'] = $request->stdAge;
+        Students::create($input);
+
+        return redirect()->route('std.index')->with('success', 'Student created successfully.');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+use App\Http\Controllers\StudentsController;
+
+use Illuminate\Support\Facades\Route;
+
+
+
+
+Route::get('/', [StudentsController::class, 'index'])->name('std.index');
+
+
+Route::post('/create-student', [StudentsController::class, 'newStudent'])->name('std.create');
